1850dc86c3a1bb76a6300ae2d3b19c099f0b4798
Rodrigo Adones <rodrigo.adonesv@gmail.com>
2021-07-18 17:49:15 -0400

    Replace home content and add about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+export default AboutPage
