9f523cf372f371d7c9ab93878ec282f54edf3e30
Rodrigo Adones <rodrigo.adonesv@gmail.com>
2021-07-18 17:59:14 -0400

    Add link component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
     <main>
       <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </main>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const IndexPage = () => {
   return (
     <main>
       <h1>Welcome to my Gatsby site!</h1>
+      <Link to="/about">About</Link>
       <p>I'm making this by following the Gatsby Tutorial.</p>
     </main>
   )
