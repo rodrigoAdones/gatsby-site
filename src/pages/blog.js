63f3ee1da5cda6669e9363955e4e23f6209f09ba
Rodrigo Adones <rodrigo.adonesv@gmail.com>
2021-07-25 11:31:30 -0400

    Get data for title with graphql (#5)
    * Get data for title with graphql
    
    * Delete swp file
    
    * Add blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BlogPage = () => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <p>This is the space for my posts.</p>
+    </Layout>
+  )
+}
+
+export default BlogPage;
