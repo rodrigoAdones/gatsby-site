9e4a37b6c152b8e06ebe92bd8d6e7d9f7dbe0426
Rodrigo Adones <rodrigo.adonesv@gmail.com>
2021-07-25 17:07:13 -0400

    Mdx posts (#6)
    * Configure file system plugin
    
    * Get mdx file names in blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,31 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <p>This is the space for my posts.</p>
+      <ul>
+        {
+          data.allFile.nodes.map(node => (
+            <li key={node.name}>
+              {node.name}
+	    </li>
+	  ))
+	}
+      </ul>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`
 
 export default BlogPage;
