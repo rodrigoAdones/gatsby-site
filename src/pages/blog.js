cf32b85ae6c6ccf316d9846a9a5f5d2ebe0b1147
rodrigoAdones <rodrigo.adones.dev@gmail.com>
2021-07-26 23:38:58 -0400

    Add content to MDX files and render them

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({ data }) => {
@@ -7,10 +8,14 @@ const BlogPage = ({ data }) => {
     <Layout pageTitle="My Blog Posts">
       <ul>
         {
-          data.allFile.nodes.map(node => (
-            <li key={node.name}>
-              {node.name}
-	    </li>
+          data.allMdx.nodes.map(node => (
+	    <article key={node.id}>
+	      <h2>{node.frontmatter.title}</h2>
+	      <p>Posted: {node.frontmatter.date}</p>
+	      <MDXRenderer>
+	        {node.body}
+	      </MDXRenderer>
+	    </article>
 	  ))
 	}
       </ul>
@@ -19,13 +24,18 @@ const BlogPage = ({ data }) => {
 }
 
 export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        name
+query {
+  allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
       }
+      id
+      body
     }
   }
+}
 `
 
 export default BlogPage;
